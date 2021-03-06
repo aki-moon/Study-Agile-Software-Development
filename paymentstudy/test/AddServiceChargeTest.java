2f678910cfdd74f9a7925bcade42ed0a89869edf
aki.m <matsuaki4@gmail.com>
2021-03-06 22:40:56 +0900

    add AddServiceChargeTest

---
+++ paymentstudy/test/AddServiceChargeTest.java
@@ -0,0 +1,12 @@
+package agilesoftwaredevelopment.paymentstudy.test;
+
+import org.junit.jupiter.api.Test;
+
+class AddServiceChargeTest {
+
+	@Test
+	void addServiceChargeTest() {
+		System.err.println("addServiceChargeTest");
+	}
+
+}
