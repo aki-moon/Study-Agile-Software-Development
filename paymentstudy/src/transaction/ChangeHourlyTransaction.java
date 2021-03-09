4f36d7d5cbae76a8b114f570e606e6df3cd1d0a4
aki.m <matsuaki4@gmail.com>
2021-03-09 21:12:00 +0900

    add ChangeHourlyTransactionTest

---
+++ paymentstudy/src/transaction/ChangeHourlyTransaction.java
@@ -0,0 +1,17 @@
+package agilesoftwaredevelopment.paymentstudy.src.transaction;
+
+import agilesoftwaredevelopment.paymentstudy.src.employee.Employee;
+
+public class ChangeHourlyTransaction extends ChangeEmployeeTransaction {
+
+	public ChangeHourlyTransaction(int empId, double rate) {
+		super(empId);
+	}
+
+	@Override
+	void change(Employee employee) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+}
--- paymentstudy/test/ChangeHourlyTransactionTest.java
+++ paymentstudy/test/ChangeHourlyTransactionTest.java
@@ -1,12 +1,27 @@
 package agilesoftwaredevelopment.paymentstudy.test;
 
+import static org.junit.Assert.*;
+
 import org.junit.jupiter.api.Test;
+
+import agilesoftwaredevelopment.paymentstudy.src.database.PayRollDataBase;
+import agilesoftwaredevelopment.paymentstudy.src.employee.Employee;
+import agilesoftwaredevelopment.paymentstudy.src.transaction.AddCommissionedEmployee;
+import agilesoftwaredevelopment.paymentstudy.src.transaction.ChangeHourlyTransaction;
 
 class ChangeHourlyTransactionTest {
 
 	@Test
 	void changeHourlyTransactionTest() {
 		System.err.println("changeHourlyTransactionTest");
+		int empId = 3;
+		AddCommissionedEmployee addCommissionedEmployee = new AddCommissionedEmployee(empId, "Lance", "Home", 2500,
+				3.2);
+		addCommissionedEmployee.execute();
+		ChangeHourlyTransaction changeHourlyTransaction = new ChangeHourlyTransaction(empId, 27.52);
+		changeHourlyTransaction.execute();
+		Employee employee = PayRollDataBase.getEmployee(empId);
+		assertNotNull(employee);
 	}
 
 }
