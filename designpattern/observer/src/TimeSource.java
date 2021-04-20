928ae61d18c2274a2821f9470d764e971d7db246
aki.m <matsuaki4@gmail.com>
2021-04-20 19:47:19 +0900

    TimeSourceに処理を委譲

--- designpattern/observer/src/TimeSource.java
+++ designpattern/observer/src/TimeSource.java
@@ -1,5 +1,19 @@
 package agilesoftwaredevelopment.designpattern.observer.src;
 
-public interface TimeSource {
-	public void registerObserver(ClockObserver driver);
+import java.util.ArrayList;
+import java.util.List;
+
+public class TimeSource {
+	private List<ClockObserver> itsObservers = new ArrayList<ClockObserver>();
+
+	public void notify(int hours, int minutes, int seconds) {
+		for (ClockObserver clockObserver : itsObservers) {
+			clockObserver.update(hours, minutes, seconds);
+		}
+	}
+
+	public void registerObserver(ClockObserver observer) {
+		itsObservers.add(observer);
+	}
+
 }
--- designpattern/observer/test/MockTimeSource.java
+++ designpattern/observer/test/MockTimeSource.java
@@ -1,22 +1,10 @@
 package agilesoftwaredevelopment.designpattern.observer.test;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import agilesoftwaredevelopment.designpattern.observer.src.ClockObserver;
 import agilesoftwaredevelopment.designpattern.observer.src.TimeSource;
 
-public class MockTimeSource implements TimeSource {
-	private List<ClockObserver> itsObservers = new ArrayList<ClockObserver>();
-
+public class MockTimeSource extends TimeSource {
 	public void setTime(int hours, int minutes, int seconds) {
-		for (ClockObserver clockObserver : itsObservers) {
-			clockObserver.update(hours, minutes, seconds);
-		}
-	}
-
-	public void registerObserver(ClockObserver observer) {
-		itsObservers.add(observer);
+		notify(hours, minutes, seconds);
 	}
 
 }
